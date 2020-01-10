284a1b8b14e7803b1990f9165bd4de89f5757337
Stefanuca Petru-Alexandeu <56625345+stefanuca@users.noreply.github.com>
2020-01-10 15:12:47 -0800

    a2

---
+++ Labirint - final/Assets/warrior.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class warrior : MonoBehaviour
+{
+    public float moveSpeed = 3f;
+    public float speedH = 2.0f;
+    private float yaw = 0.0f;
+    public GameObject text;
+    public Animator animator;
+    private float movement = 0f;
+        // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        movement = Input.GetAxisRaw("Vertical") * moveSpeed;
+        animator.SetFloat("Speed", Mathf.Abs(movement));
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            text.SetActive(false);
+        }
+
+        //Moves Forward and back along z axis                           //Up/Down
+        transform.Translate(Vector3.forward * Time.deltaTime * Input.GetAxis("Vertical") * moveSpeed);
+        //Moves Left and right along x Axis                               //Left/Right
+        transform.Translate(Vector3.right * Time.deltaTime * Input.GetAxis("Horizontal") * moveSpeed);
+
+        yaw += speedH * Input.GetAxis("Mouse X");
+        transform.eulerAngles = new Vector3(0, yaw, 0);
+
+
+    }
+}
