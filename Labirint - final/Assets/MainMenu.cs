039b8e517a568c597cdba8873cb9a700ad6bfa20
Stefanuca Petru-Alexandeu <56625345+stefanuca@users.noreply.github.com>
2020-01-14 21:26:54 -0800

    am adaugat meniul

---
+++ Labirint - final/Assets/MainMenu.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void changescene(string scenename)
+    {
+        Application.LoadLevel(scenename); 
+    }
+}
